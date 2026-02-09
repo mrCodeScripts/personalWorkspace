b96a606e726f9c63e49d765bf2b7c0d98d058d2d
mrCodeScripts <kqlyshock@gmail.com>
2026-02-10 00:14:48 +0800

    I just learned useId in ReactJS

--- webDev/proj_01/frontend/app/components/examples/HooksAndExamples/useId.tsx
+++ webDev/proj_01/frontend/app/components/examples/HooksAndExamples/useId.tsx
@@ -1,28 +1,49 @@
 "use client";
 import { useId, useEffect, useState } from "react";
 
-function UseIdExample1 () {
-  const [password, setPassword] = useState<string>("");
-  const username_inpt_id: string = useId();
-  const password_input_id: string = useId();
+function UseIdExample1() {
+  const InputLabelComponent = () => {
+    const [inputVal, setInputVal] = useState<string>("");
+    const inpt_id: string = useId();
+    useEffect(() => {
+      console.log(`Input value of ${inpt_id}: `, inputVal);
+      return () => console.log("Clean-up");
+    }, [inputVal]);
 
-  useEffect(() => {
-    console.log("Password input: ", password);
-    return () => console.log("Clean-up");
-  }, [password]);
-
-  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
+    const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setInputVal(e.target.value);
+    };
+    return (
+      <div style={{display: "flex", flexDirection: "column"}}>
+        <label htmlFor={inpt_id} style={{ color: "#303030" }}>
+          Input Username:{" "}
+        </label>
+        <input
+          type="text"
+          id={inpt_id}
+          placeholder="Username..."
+          style={{
+            border: "1px solid #303030",
+            backgroundColor: "#F1F1F1",
+            borderRadius: "5px",
+            padding: "10px",
+          }}
+          value={inputVal}
+          onChange={handleInput}
+        />
+      </div>
+    );
   };
 
   return (
     <>
-      <form style={{display: "flex", flexDirection: "column", width: "300px"}}>
-        <label htmlFor={username_inpt_id} style={{color: "#303030"}}>Input Username: </label>
-        <input type="text" id={username_inpt_id} placeholder="Username..." style={{border: "1px solid #303030", backgroundColor: "#F1F1F1", borderRadius: "5px", padding: "10px"}} value={password!} onChange={handleInput}/>
+      <form style={{ display: "flex", flexDirection: "column", width: "300px", gap: "10px"}} >
+        <InputLabelComponent />
+        <InputLabelComponent />
+        <InputLabelComponent />
       </form>
     </>
-  )
+  );
 }
 
 export default function UseIdExamples() {
@@ -30,5 +51,5 @@ export default function UseIdExamples() {
     <>
       <UseIdExample1 />
     </>
-  )
+  );
 }
