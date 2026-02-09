79770bc5ea3a59dd4d021a95e44327a31952a04f
fredchuckz1217 <liborwilfred@gmail.com>
2026-02-09 13:21:24 +0800

    lksjdfl;jasfl;kj

--- webDev/proj_01/frontend/app/components/examples/HooksAndExamples/forwardRef.tsx
+++ webDev/proj_01/frontend/app/components/examples/HooksAndExamples/forwardRef.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { forwardRef, useEffect, useRef } from "react";
+import { forwardRef, useEffect, useEffectEvent, useRef } from "react";
 
 const FowardThis = forwardRef<
   HTMLButtonElement,
@@ -28,27 +28,90 @@ function ForwardRefExample1() {
         }}
         ref={buttonElement_1}
       />
-      <button className="btn" onClick={() => buttonElement_1.current!.style.backgroundColor == "red" ? buttonElement_1.current!.style.backgroundColor = "green" : buttonElement_1.current!.style.backgroundColor = "red"}>
+      <button
+        className="btn"
+        onClick={() =>
+          buttonElement_1.current!.style.backgroundColor == "red"
+            ? (buttonElement_1.current!.style.backgroundColor = "green")
+            : (buttonElement_1.current!.style.backgroundColor = "red")
+        }
+      >
         This is a Dev Element
       </button>
     </>
   );
-};
+}
 
 function ForwardRefExample2() {
-  
+  const i = useRef(0);
+  const divElement = useRef<HTMLDivElement | null>(null);
+
+  const handleCount = () => {
+    i.current++;
+    console.log(i.current);
+    divElement.current!.style.transform = `translateX(${i.current * 20}px)`;
+  };
 
   return (
     <>
-
+      <button onClick={handleCount}>Click This!</button>
+      <div
+        ref={divElement}
+        className="p-10 bg-red-500 duration-300 ease-in-out"
+      >
+        This element!
+      </div>
     </>
   );
-};
+}
+
+function SnakeGame() {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const canvasWidth = 300;
+  const canvasHeight = 300;
+
+  useEffect(() => {
+    let ctx = canvasRef.current!.getContext("2d");
+    let animation: number;
+
+    const renderBlock = (
+      ctx: CanvasRenderingContext2D,
+      width: number,
+      height: number,
+      blockX: number,
+      blockY: number,
+      blockW: number,
+      blockH: number,
+      blockColor: string,
+    ) => {
+      ctx.beginPath();
+      ctx.rect(blockX, blockY, blockW, blockH);
+      ctx.fillStyle = blockColor;
+      ctx.fill();
+      ctx.closePath();
+    };
+
+    
+
+    const run = () => {
+      animation = requestAnimationFrame(run);
+    };
+
+    return () => cancelAnimationFrame(animation);
+  }, []);
+
+  return (
+    <>
+      <canvas ref={canvasRef} />
+    </>
+  );
+}
 
 export default function ForwardRefExamples() {
   return (
     <>
-      <ForwardRefExample1 />
+      {/* <ForwardRefExample1 /> */}
+      <ForwardRefExample2 />
     </>
   );
 }
