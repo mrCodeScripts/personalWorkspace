ab63fe89fe92bf53e6921a7ec9e225850b5b4cdc
fredchuckz1217 <liborwilfred@gmail.com>
2026-02-09 14:13:34 +0800

    I made a snake game using React

--- webDev/proj_01/frontend/app/components/examples/HooksAndExamples/forwardRef.tsx
+++ webDev/proj_01/frontend/app/components/examples/HooksAndExamples/forwardRef.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { request } from "http";
 import { forwardRef, useEffect, useEffectEvent, useRef } from "react";
 
 const FowardThis = forwardRef<
@@ -67,17 +68,23 @@ function ForwardRefExample2() {
 
 function SnakeGame() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const canvasWidth = 300;
-  const canvasHeight = 300;
+  const prevTime = useRef<number>(performance.now());
+  const accumilator = useRef<number>(0);
+  const canvasWidth = 800;
+  const canvasHeight = 800;
 
   useEffect(() => {
-    let ctx = canvasRef.current!.getContext("2d");
+    let FIXED_TIMESTAMP: number = 0.1;
+    canvasRef.current!.width = canvasWidth;
+    canvasRef.current!.height = canvasHeight;
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+    const ctx = canvas!.getContext("2d");
+    if (!ctx) return;
     let animation: number;
 
     const renderBlock = (
       ctx: CanvasRenderingContext2D,
-      width: number,
-      height: number,
       blockX: number,
       blockY: number,
       blockW: number,
@@ -91,13 +98,226 @@ function SnakeGame() {
       ctx.closePath();
     };
 
-    
+    type dir = { x: number; y: number };
+    let snakeDir: dir = { x: 1, y: 0 };
+    let snakeSegment: dir[] = [
+      { x: 1, y: 1 },
+      { x: 2, y: 1 },
+      { x: 3, y: 1 },
+    ];
 
-    const run = () => {
+    const map: number[][] = [
+      [
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0,
+      ],
+      [
+        0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 0,
+      ],
+      [
+        0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 0,
+      ],
+      [
+        0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 0,
+      ],
+      [
+        0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 0,
+      ],
+      [
+        0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 0,
+      ],
+      [
+        0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 0,
+      ],
+      [
+        0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 0,
+      ],
+      [
+        0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 0,
+      ],
+      [
+        0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 0,
+      ],
+      [
+        0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 0,
+      ],
+      [
+        0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 0,
+      ],
+      [
+        0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 0,
+      ],
+      [
+        0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 0,
+      ],
+      [
+        0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 0,
+      ],
+      [
+        0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 0,
+      ],
+      [
+        0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 0,
+      ],
+      [
+        0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 0,
+      ],
+      [
+        0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 0,
+      ],
+      [
+        0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 0,
+      ],
+      [
+        0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 0,
+      ],
+      [
+        0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 0,
+      ],
+      [
+        0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 0,
+      ],
+      [
+        0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 0,
+      ],
+      [
+        0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 0,
+      ],
+      [
+        0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 0,
+      ],
+      [
+        0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 0,
+      ],
+      [
+        0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 0,
+      ],
+      [
+        0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 0,
+      ],
+      [
+        0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 0,
+      ],
+      [
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0,
+      ],
+    ];
+
+    const updateSnake  = (snakeDirs: dir[], snakeDir: dir) => {
+      // for (let i: number = 0; i < snakeDirs.length; i++) {
+      // }
+      let newPart: dir = {x: snakeDirs[0].x + snakeDir.x, y: snakeDirs[0].y + snakeDir.y};
+      snakeDirs.pop();
+      snakeDirs.unshift(newPart);
+    };
+
+    const detectKeyboard = (e: KeyboardEvent) => {
+      let newSnakeDir: dir = snakeDir;
+      switch (e.key) {
+        case "ArrowUp":
+          newSnakeDir.x = 0;
+          newSnakeDir.y = -1;
+          break;
+        case "ArrowDown":
+          newSnakeDir.x = 0;
+          newSnakeDir.y = 1;
+          break;
+        case "ArrowLeft":
+          newSnakeDir.x = -1;
+          newSnakeDir.y = 0;
+          break;
+        case "ArrowRight":
+          newSnakeDir.x = 1;
+          newSnakeDir.y = 0;
+          break;
+        default:
+          console.log("Invalid Keyboard Input");
+          break;
+      }
+      snakeDir = newSnakeDir;
+    };
+
+
+    const renderMap = (mapMatrix: number[][]) => {
+      for (let i: number = 0; i < mapMatrix.length; i++) {
+        for (let j: number = 0; j < mapMatrix[0].length; j++) {
+          const blockWidth = canvasWidth / mapMatrix[0].length;
+          const blockHeight = canvasHeight / mapMatrix.length;
+          const blockX = j * blockWidth;
+          const blockY = i * blockHeight;
+
+          let snakeBodyDetected: boolean = false;
+          for (
+            let snakeSeg: number = 0;
+            snakeSeg < snakeSegment.length;
+            snakeSeg++
+          ) {
+            if (i == snakeSegment[snakeSeg].y && j == snakeSegment[snakeSeg].x)
+              snakeBodyDetected = true;
+          }
+
+          if (mapMatrix[i][j] == 0 && !snakeBodyDetected) {
+            renderBlock(ctx, blockX, blockY, blockWidth, blockHeight, "red");
+          } else if (mapMatrix[i][j] == 1 && !snakeBodyDetected) {
+            renderBlock(ctx, blockX, blockY, blockWidth, blockHeight, "black");
+          } else if (snakeBodyDetected) {
+            renderBlock(ctx, blockX, blockY, blockWidth, blockHeight, "green");
+          }
+        }
+      }
+    };
+
+    const run = (currentTime: number) => {
+      const deltaTime: number = (currentTime - prevTime.current) / 1000;
+      prevTime.current = currentTime;
+      accumilator.current += deltaTime;
+      window.addEventListener("keydown", (e: KeyboardEvent) => detectKeyboard(e));
+      while (accumilator.current >= FIXED_TIMESTAMP) {
+        console.log("run");
+        ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+        updateSnake(snakeSegment, snakeDir);
+        renderMap(map);
+        accumilator.current -= FIXED_TIMESTAMP;
+      }
       animation = requestAnimationFrame(run);
     };
 
-    return () => cancelAnimationFrame(animation);
+    requestAnimationFrame(run);
+
+    return () => {
+      window.removeEventListener("keydown", detectKeyboard);
+      cancelAnimationFrame(animation);
+    }
   }, []);
 
   return (
@@ -111,7 +331,8 @@ export default function ForwardRefExamples() {
   return (
     <>
       {/* <ForwardRefExample1 /> */}
-      <ForwardRefExample2 />
+      {/* <ForwardRefExample2 /> */}
+      <SnakeGame />
     </>
   );
 }
