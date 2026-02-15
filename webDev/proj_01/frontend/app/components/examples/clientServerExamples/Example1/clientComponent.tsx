eba53881a01c94db734022825742a678fea595ca
mrCodeScripts <kqlyshock@gmail.com>
2026-02-15 18:56:58 +0800

    lksjfl;j

--- webDev/proj_01/frontend/app/components/examples/clientServerExamples/Example1/clientComponent.tsx
+++ webDev/proj_01/frontend/app/components/examples/clientServerExamples/Example1/clientComponent.tsx
@@ -9,12 +9,33 @@ export default function ClientSideComp() {
   };
 
   const [formState, formAction, isPending] = useActionState<FormType, FormData>(async (prev, formData) => {
+
+    const name = String(formData.get("name"));
+    const age = Number(formData.get("age"));
+
+    const path = "http://localhost:3001/api/userSubmit";
+    const payload: FormType = {name: name, age: age};
+
+    const req = await fetch(path, {
+      method: 'POST',
+      headers: {'Content-Type':'application/json'},
+      body: JSON.stringify(payload)
+    });
+
+    const res = await req.json();
+
     return {name: "", age: 0};
   }, {name: "", age: undefined});
 
   return (
     <>
-      <div></div>
+      <div>
+        <form action={formAction}>
+          <input type="text" name="name" placeholder="Enter username..." disabled={isPending} />
+          <input type="number" name="age" placeholder="Enter age..." disabled={isPending}/>
+          <button type="submit" disabled={isPending}>{isPending ? 'Submitting...' : 'Submit'}</button>
+        </form>
+      </div>
     </>
   );
 }
